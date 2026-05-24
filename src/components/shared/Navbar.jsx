d2835201516985b4162a69cdeca93deae417703d
omarfaruk <mdomarfaruq2806@gmail.com>
2026-05-24 18:57:50 +0600

    navbar dropdown added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const router = useRouter();
 
   const [openmenu, setOpenMenu] = useState(false);
-  const [profileDropdwon , setProfileDropdown] = useState(false);
+  const [profileDropdwon, setProfileDropdown] = useState(false);
 
   const {
     data: session,
@@ -52,36 +52,51 @@ const Navbar = () => {
   };
 
   const authButtons = session ? (
-    <div className="flex gap-2 flex-col lg:flex-row items-center">
-      <p className="font-bold text-lg text-secondary">
-        Hi , {session?.user?.name}
-      </p>
-      <button
-        className="px-3 py-1 rounded-full border border-red-400 text-red-400 hover:bg-red-400 hover:text-white transition flex items-center gap-2"
-        onClick={signout}
+    <div className="dropdown dropdown-center  lg:dropdown-end">
+      {/* Trigger */}
+      <div
+        tabIndex={0}
+        role="button"
+        className="bg-secondary text-white px-4 py-2 rounded-full flex items-center gap-2 cursor-pointer"
       >
-        <GoSignOut />
-        Sign Out
-      </button>
+        <span className="font-bold text-sm lg:text-base">
+          Hi, {session?.user?.name}
+        </span>
+      </div>
+
+      {/* Dropdown */}
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu bg-accent rounded-box z-[999] mt-3 p-4 shadow w-50 lg:w-64 flex flex-col items-center gap-3"
+      >
+        <Image
+          src={session?.user?.image || "/hero.jpg"}
+          width={60}
+          height={60}
+          alt="profile"
+          className="rounded-full border-4 border-secondary object-cover"
+          referrerPolicy="no-referrer"
+        />
+
+        <p className="text-xs text-center break-all">{session?.user?.email}</p>
+
+        <button
+          className="w-full px-3 py-2 rounded-full border border-red-400 text-red-400 hover:bg-red-400 hover:text-white transition flex items-center justify-center gap-2"
+          onClick={signout}
+        >
+          <GoSignOut />
+          Sign Out
+        </button>
+      </ul>
     </div>
   ) : (
-    // <details class="$$dropdown">
-    //   <summary class="$$btn m-1">open or close</summary>
-    //   <ul class="$$menu $$dropdown-content bg-base-100 $$rounded-box z-1 w-52 p-2 shadow-sm">
-    //     <li>
-    //       <a>Item 1</a>
-    //     </li>
-    //     <li>
-    //       <a>Item 2</a>
-    //     </li>
-    //   </ul>
-    // </details>
     <div className="flex flex-col lg:flex-row gap-3 items-center">
       <Link href="/login">
         <button className="border text-secondary px-4 py-2 rounded-full w-24">
           Log In
         </button>
       </Link>
+
       <Link href="/register">
         <button className="bg-secondary text-white px-4 py-2 rounded-full w-24">
           Register
@@ -93,16 +108,7 @@ const Navbar = () => {
   return (
     <div className="shadow-md shadow-secondary/10 bg-[#F8FBFF] sticky top-0 z-50">
       <nav className="flex items-center justify-between p-4 font-semibold  lg:container mx-auto">
-        <div className=" text-3xl font-bold text-secondary">
-          {/* <Image
-            src="/logo.png"
-            alt="logo"
-            width={100}
-            height={100}
-            className=" h-16 object-contain"
-          ></Image> */}
-          STUDYNOOK
-        </div>
+        <div className=" text-3xl font-bold text-secondary">STUDYNOOK</div>
         <button
           onClick={() => setOpenMenu(!openmenu)}
           className="text-right lg:hidden text-2xl text-secondary"
@@ -119,15 +125,10 @@ const Navbar = () => {
         {/* mobile menu */}
         {/* {isPending && <p>Loading...</p>} */}
         {openmenu && (
-          <ul className="flex flex-col gap-4 absolute top-18 right-1  mx-auto text-center p-4 rounded-lg shadow-md lg:hidden w-50 bg-[#F8FBFF] transition">
+          <ul className="flex flex-col items-center gap-4 absolute top-20 right-4 w-56 p-4 rounded-lg shadow-lg lg:hidden bg-[#F8FBFF] z-50">
             {renderLinks(() => setOpenMenu(false))}
-            <div
-              onClick={() => {
-                setOpenMenu(false);
-              }}
-            >
-              {authButtons}
-            </div>
+
+            <div className="mt-2 flex flex-col items-center">{authButtons}</div>
           </ul>
         )}
       </nav>
